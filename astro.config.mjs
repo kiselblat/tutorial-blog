02ee06188b2aa505e2fe4d0f4d3ee1a13c1bdc72
Tom Christ <44477304+kiselblat@users.noreply.github.com>
2023-12-12 14:22:41 -0600

    Adds preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tutorial-blog-tsec85.netlify.app/"
+  site: "https://tutorial-blog-tsec85.netlify.app/",
+  integrations: [preact()]
 });
